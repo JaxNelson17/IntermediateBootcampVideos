1ce8233c3baa457a3bdc74f46f48db3c7631d711
JaxNelson17 <jaxnel17@gmail.com>
2022-02-14 12:59:16 -0700

    Initial commit

---
+++ IntermediateBootcamp/IntermediateBootcamp/IntermediateBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateBootcampApp.swift
+//  IntermediateBootcamp
+//
+//  Created by Jaxson Nelson on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
